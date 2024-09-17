bee2321cc6782ffe95651b0dd1a5d8e813bca499
eroeb <97666589+eroeb@users.noreply.github.com>
2024-09-17 13:18:01 +0200

    confirm delete

--- src/database/DatabaseComponents.tsx
+++ src/database/DatabaseComponents.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { deleteFileFromDB, getFilesFromIndexedDB } from "./database.ts";
+import { Button, Modal } from "react-bootstrap";
 
 
 export const ResetDBButton: React.FC = () => {
@@ -9,7 +10,7 @@ export const ResetDBButton: React.FC = () => {
                 indexedDB.deleteDatabase('UserFilesDB');
             });
             console.log('Database reset successfully');
-        } catch (error) {
+        } catch(error) {
             console.error('Error resetting database:', error);
         }
     };
@@ -40,19 +41,57 @@ export function AudioFileList() {
         };
     }, []);
 
+
     return (
         <div>
-            <ul>
-                {files.map((file, index) => (
-                    <li key={index}>
-                        {file.name}
-                        <audio controls src={URL.createObjectURL(file)} />
-                        <button onClick={() => deleteFileFromDB(file, 'audioFiles')}>
-                            Delete
-                        </button>
-                    </li>
-                ))}
-            </ul>
+            <div className="d-flex flex-wrap">
+                {files.map((file, index) => <AudioFile key={file.name} file={file} />
+                )}
+            </div>
         </div>
     );
+}
+
+function AudioFile({ file }) {
+    const [showConfirmDialog, setShowConfirmDialog] = useState(false);
+
+    return <div className="card m-2" style={{ width: '18rem' }}>
+        <div className="card-body">
+            <h5 className="card-title">{file.name}</h5>
+            <audio controls src={URL.createObjectURL(file)} className="w-100" />
+            <button
+                className="btn btn-danger mt-2"
+                onClick={() => setShowConfirmDialog(true)}
+            >
+                Delete
+            </button>
+            <ConfirmDelete file={file} show={showConfirmDialog} setShow={setShowConfirmDialog} />
+        </div>
+    </div>
+}
+
+function ConfirmDelete({ file, show, setShow }) {
+    return (
+        <Modal show={show ? show : false} onHide={() => setShow(false)}>
+            <Modal.Header closeButton>
+                <Modal.Title>Confirm Delete</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <p>Do you really want to delete file <br />
+                    <b>{file.name}</b>?<br />
+                    You will not be able to play or show it in any quiz and you cannot recover it.
+                </p>
+
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={() => setShow(false)}>
+                    Cancel
+                </Button>
+                <Button variant="danger" onClick={() => deleteFileFromDB(file, "audioFiles")}>
+                    Delete
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
+
 }
