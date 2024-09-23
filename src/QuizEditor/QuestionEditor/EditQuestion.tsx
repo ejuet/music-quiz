af2f78d487512859de7099c9881539e4ed67f592
eroeb <97666589+eroeb@users.noreply.github.com>
2024-09-23 23:30:16 +0200

    format

--- src/QuizEditor/QuestionEditor/EditQuestion.tsx
+++ src/QuizEditor/QuestionEditor/EditQuestion.tsx
@@ -16,73 +16,73 @@ export function EditQuestion({ question }: { question: Question }) {
             <SimpleQuestionEditor question={question} />
         }
         <Form.Label>Category</Form.Label>
-        <EditCategory category={question.category} onChange={(value) => question.category = value}/>
-        <DeleteQuestionButton question={question}/>
+        <EditCategory category={question.category} onChange={(value) => question.category = value} />
+        <DeleteQuestionButton question={question} />
     </Card>
 }
 
 function SimpleQuestionEditor({ question }: { question: SimpleQuestion }) {
     return <>
         <Form.Label>Question</Form.Label>
-        <EditText text={question.question} onChange={(value) => question.question = value}/>
+        <EditText text={question.question} onChange={(value) => question.question = value} />
         <Form.Label>Song</Form.Label>
-        <EditSong song={question.song} onChange={(value) => question.song = value}/>
+        <EditSong song={question.song} onChange={(value) => question.song = value} />
         <Form.Label>Answer</Form.Label>
-        <EditText text={question.answer} onChange={(value) => question.answer = value}/>
+        <EditText text={question.answer} onChange={(value) => question.answer = value} />
         <Form.Label>Points</Form.Label>
-        <EditNumber number={question.pointsIfRight} onChange={(value) => question.pointsIfRight = value}/>
+        <EditNumber number={question.pointsIfRight} onChange={(value) => question.pointsIfRight = value} />
     </>
 }
 
 function EditSong({ song, onChange }: { song: PlayableSong, onChange: (value: PlayableSong) => void }) {
-    const {setAppData, appData} = useAppDataContext();
+    const { setAppData, appData } = useAppDataContext();
     const currentQuiz = useCurrentQuiz();
     const audioFiles = useAudioFiles();
     return <>
         <ButtonGroup>
-        <Dropdown>
-            <Dropdown.Toggle id="dropdown-basic">
-                {song.filename!="" ? song.filename : "Select a song"}
-            </Dropdown.Toggle>
+            <Dropdown>
+                <Dropdown.Toggle id="dropdown-basic">
+                    {song.filename != "" ? song.filename : "Select a song"}
+                </Dropdown.Toggle>
 
-            <Dropdown.Menu>
-                <Dropdown.Item as="div">
-                    <UploadSoundFile onUpload={(file)=>{
-                        onChange({ filename: file.name });
-                        setAppData(appData);
-                    }} />
-                </Dropdown.Item>
-                {
-                    audioFiles.map((file) => {
-                        return <Dropdown.Item key={file.name} onClick={() => {
+                <Dropdown.Menu>
+                    <Dropdown.Item as="div">
+                        <UploadSoundFile onUpload={(file) => {
                             onChange({ filename: file.name });
                             setAppData(appData);
-                        }}>{file.name}</Dropdown.Item>
-                    })
-                }
-            </Dropdown.Menu>
-        </Dropdown>
-        {
-            song.filename !== "" &&
-            <Button variant="secondary" style={{maxWidth: '100px'}}
-            onClick={()=>{
-                onChange({ filename: '' });
-                setAppData(appData);
-            }}
-        >
-            Clear
-        </Button>
-        }
-        
+                        }} />
+                    </Dropdown.Item>
+                    {
+                        audioFiles.map((file) => {
+                            return <Dropdown.Item key={file.name} onClick={() => {
+                                onChange({ filename: file.name });
+                                setAppData(appData);
+                            }}>{file.name}</Dropdown.Item>
+                        })
+                    }
+                </Dropdown.Menu>
+            </Dropdown>
+            {
+                song.filename !== "" &&
+                <Button variant="secondary" style={{ maxWidth: '100px' }}
+                    onClick={() => {
+                        onChange({ filename: '' });
+                        setAppData(appData);
+                    }}
+                >
+                    Clear
+                </Button>
+            }
+
         </ButtonGroup>
-        
+
         <PlayAudio filename={song.filename} />
-        
-    </> 
+
+    </>
 }
 
 function DeleteQuestionButton({ question }: { question: any }) {
-    const {setAppData, appData} = useAppDataContext();
+    const { setAppData, appData } = useAppDataContext();
     const currentQuiz = useCurrentQuiz();
 
     return <Button variant="danger" className="mt-1" onClick={() => {
@@ -92,7 +92,7 @@ function DeleteQuestionButton({ question }: { question: any }) {
 }
 
 export function EditText({ text, onChange }: { text: DisplayableText, onChange: (value: string) => void }) {
-    const {setAppData, appData} = useAppDataContext();
+    const { setAppData, appData } = useAppDataContext();
     const handleChange = (e) => {
         onChange(e.target.value);
         setAppData(appData);
@@ -100,23 +100,23 @@ export function EditText({ text, onChange }: { text: DisplayableText, onChange: 
 
     const textToEdit = typeof text === 'string' ? text : text.text;
 
-    return <TextareaAutosize className="form-control" value={textToEdit} onChange={handleChange}/>
+    return <TextareaAutosize className="form-control" value={textToEdit} onChange={handleChange} />
 }
 
 export function EditNumber({ number, onChange }: { number: number, onChange: (value: number) => void }) {
-    const {setAppData, appData} = useAppDataContext();
+    const { setAppData, appData } = useAppDataContext();
     const handleChange = (e) => {
         const value = parseInt(e.target.value);
         onChange(value);
         setAppData(appData);
     };
 
-    return <input 
-        className="form-control" 
-        defaultValue={number} 
-        onBlur={handleChange} 
+    return <input
+        className="form-control"
+        defaultValue={number}
+        onBlur={handleChange}
         onKeyUp={(e) => {
-            if (e.key === 'Enter') {
+            if(e.key === 'Enter') {
                 handleChange(e);
             }
         }}
@@ -124,16 +124,16 @@ export function EditNumber({ number, onChange }: { number: number, onChange: (va
             const inputValue = (e.target as HTMLInputElement).value.replace(/[^0-9]/g, '');
             (e.target as HTMLInputElement).value = inputValue;
         }}
-        style={{ 
-            appearance: 'textfield', 
-            MozAppearance: 'textfield', 
-            width: `${number.toString().length + 3 +2}ch` 
+        style={{
+            appearance: 'textfield',
+            MozAppearance: 'textfield',
+            width: `${number.toString().length + 3 + 2}ch`
         }}
     />
 }
 
 function EditCategory({ category, onChange }: { category: string, onChange: (value: string) => void }) {
-    const {setAppData, appData} = useAppDataContext();
+    const { setAppData, appData } = useAppDataContext();
     const currentQuiz = useCurrentQuiz();
 
     return <Dropdown>
