386c07210f490331c31876a89816c3187862f9e9
eroeb <97666589+eroeb@users.noreply.github.com>
2024-12-08 19:03:48 +0100

    game play

--- src/Play/PlayGame.tsx
+++ src/Play/PlayGame.tsx
@@ -1,11 +1,93 @@
 import { Button } from "react-bootstrap";
 import React from "react";
 import { useAppDataContext, useCurrentGame, useCurrentQuiz } from "../Logic/AppDataContext.tsx";
-import { AnswerQuestion, SelectQuestion } from "../Logic/gameStructure.ts";
+import { AnswerQuestion, GameAction, SelectQuestion, TeamAction } from "../Logic/gameStructure.ts";
 
 export function PlayGame(){
+    const currentGame = useCurrentGame();
+    const currentQuiz = useCurrentQuiz();
+    const { appData, setAppData } = useAppDataContext();
+    if(!currentGame){
+        return <h1>Game not found</h1>;
+    }
+    if(!currentQuiz){
+        return <h1>Quiz not found</h1>;
+    }
+    const nextAction = currentGame.getNextAction();
+
     return <>
+        {
+            currentGame.gameActions.length === 0  &&
+            <Button onClick={() => {
+                currentGame.startGame(currentQuiz);
+                setAppData(appData);
+            }}>Start Game</Button>
+        }
+        {
+            nextAction &&
+            <DisplayAction action={nextAction} />
+        }
     </>
+}
+
+function DisplayAction({ action }: { action:GameAction }) {
+    return <div>
+        {
+            action.actionType === "SelectQuestion" &&
+            <SelectQuestionAction action={action as SelectQuestion} />
+        }
+        {
+            action.actionType === "AnswerQuestion" &&
+            <AnswerQuestionAction action={action as AnswerQuestion} />
+        }
+    </div>
+}
+
+function useCurrentTeam(){
+    const currentGame = useCurrentGame();
+    const currentAction = currentGame?.getNextAction();
+    if(!currentAction || !currentGame){
+        return undefined;
+    }
+    if(!(currentAction as TeamAction).teamId){
+        return undefined;
+    }
+    return currentGame?.teams.find(t => t.id === (currentAction as TeamAction).teamId);
+}
+
+function SelectQuestionAction({ action }: { action: SelectQuestion }) {
+    const currentQuiz = useCurrentQuiz();
+    const currentGame = useCurrentGame();
+    const team = useCurrentTeam();
+    const { appData, setAppData } = useAppDataContext();
+
+    return <div>
+        <h1>Select Question</h1>
+        <p>Team <b>{team?.name}</b> has to select their next question!</p>
+        {
+            currentQuiz?.items.map((q) => {
+                if(action.availableQuestions.includes(q.questionId)){
+                    return <Button onClick={() => {
+                        action.questionId = q.questionId;
+                        setAppData(appData);
+                    }}>{q.questionId} {q.getPoints()} Punkte</Button>
+                }
+            })
+        }
+    </div>
+}  
+
+function AnswerQuestionAction({ action }: { action: AnswerQuestion }) {
+    const team = useCurrentTeam();
+    const { appData, setAppData } = useAppDataContext();
+    return <div>
+        <h1>Answer Question</h1>
+        <p>Team <b>{team?.name}</b> has to answer the question!</p>
+        <Button onClick={() => {
+            action.finished = true;
+            setAppData(appData);
+        }}>Finish</Button>
+    </div>
 }
 
 export function GameHistory(){
